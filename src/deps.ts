aa73a446dbdc54b72a0a90d67a178ab3cd78f905
Filip Maj <fmaj@slack-corp.com>
2022-07-19 15:25:07 -0400

    Bump builder dependency to 0.0.14 to avoid pulling in the entire collections deno stdlib. This eliminates an additional warning in stdout in the lambda execution context as well as slimming down this library by ~800 kb.

--- src/deps.ts
+++ src/deps.ts
@@ -1,4 +1,4 @@
 export { readAll } from "https://deno.land/std@0.99.0/io/util.ts";
 export { BaseSlackAPIClient } from "https://deno.land/x/deno_slack_api@0.0.2/base-client.ts";
-export { createManifest } from "https://deno.land/x/deno_slack_builder@0.0.7/manifest.ts";
+export { createManifest } from "https://deno.land/x/deno_slack_builder@0.0.14/manifest.ts";
 export { parse } from "https://deno.land/std@0.99.0/path/mod.ts";
