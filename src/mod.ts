1fa70bc434a7f60c303e1166c71da29447cc2fb4
Brad Harris <bharris@slack-corp.com>
2022-08-08 14:54:11 -0600

    trying builtin serve

--- src/mod.ts
+++ src/mod.ts
@@ -1,6 +1,6 @@
 import { DispatchPayload } from "./dispatch-payload.ts";
 import { InvocationPayload } from "./types.ts";
-import { parse } from "./deps.ts";
+import { parse, serve } from "./deps.ts";
 
 export const run = async function (functionDir: string, input: string) {
   // Directory containing functions must be provided when invoking this script.
@@ -21,77 +21,40 @@ export const run = async function (functionDir: string, input: string) {
   return resp || {};
 };
 
-// Start a http server that listens on the provided port
-// This server exposes two routes
-//  GET  `/health`    Returns a 200 OK
-//  POST `/functions` Accepts event payload and invokes `run`
-const startServer = async function (port: number) {
-  let server;
-  try {
-    server = Deno.listen({ port });
-  } catch (e) {
-    throw new Error(`Unable to listen on ${port}, got ${e}`);
-  }
-  for await (const conn of server) {
-    // In order to not be blocking, we need to handle each connection individually
-    // without awaiting the function
-    serveHttp(conn);
-  }
+const handleRequest = async (request: Request) => {
+  const url = new URL(request.url);
 
-  async function serveHttp(conn: Deno.Conn) {
-    // This "upgrades" a network connection into an HTTP connection.
-    const httpConn = Deno.serveHttp(conn);
-    // Each request sent over the HTTP connection will be yielded as an async
-    // iterator from the HTTP connection.
-    for await (const requestEvent of httpConn) {
-      // The native HTTP server uses the web standard `Request` and `Response`
-      // objects.
+  // A health check route
+  if (request.method == "GET" && url.pathname == "/health") {
+    return new Response("OK", {
+      status: 200,
+    });
+  } else if (
+    request.method == "POST" && url.pathname == "/functions"
+  ) {
+    const body = await request.text();
 
-      const url = new URL(requestEvent.request.url);
-      // A health check route
-      if (requestEvent.request.method == "GET" && url.pathname == "/health") {
-        await requestEvent.respondWith(
-          new Response("OK", {
-            status: 200,
-          }),
-        ).catch((e) =>
-          console.log(`Uncaught exception during health check: ${e}`)
-        );
-      } else if (
-        requestEvent.request.method == "POST" && url.pathname == "/functions"
-      ) {
-        const body = await requestEvent.request.text();
-        try {
-          // run the user code
-          const response = await run("functions", body);
-          await requestEvent.respondWith(
-            new Response(JSON.stringify(response), {
-              status: 200,
-              headers: { "Content-Type": "application/json" },
-            }),
-          ).catch((e) =>
-            console.log(`Uncaught exception after running user code: ${e}`)
-          );
-        } catch (e) {
-          console.error(`Unable to run user supplied module caught error ${e}`);
-          await requestEvent.respondWith(
-            new Response(`error ${e}`, {
-              status: 500,
-            }),
-          ).catch((e) => console.log(`Uncaught exception: ${e}`));
-        }
-      } else {
-        // catch all for any unexpected route
-        await requestEvent.respondWith(
-          new Response(
-            `error unknown route ${requestEvent.request.method} ${url.pathname}`,
-            { status: 404 },
-          ),
-        ).catch((e) =>
-          console.log(`Uncaught exception on calling unexpected routes: ${e}`)
-        );
-      }
+    try {
+      // run the user code
+      const response = await run("functions", body);
+
+      return new Response(JSON.stringify(response), {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      });
+    } catch (e) {
+      console.error(`Unable to run user supplied module caught error ${e}`);
+
+      return new Response(`error ${e}`, {
+        status: 500,
+      });
     }
+  } else {
+    // catch all for any unexpected route
+    return new Response(
+      `error unknown route ${request.method} ${url.pathname}`,
+      { status: 404 },
+    );
   }
 };
 
@@ -106,5 +69,5 @@ if (import.meta.main) {
     throw Error("port must be number");
   }
 
-  await startServer(port);
+  await serve(handleRequest, { port });
 }
