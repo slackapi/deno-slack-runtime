dcfc7e319be993ac254392def4b37b34935bdc74
Brad Harris <bharris@slack-corp.com>
2022-08-09 16:43:02 -0600

    log statements

--- src/mod.ts
+++ src/mod.ts
@@ -15,6 +15,7 @@ const startServer = async (port: number, timeout: number) => {
   } catch (e) {
     throw new Error(`Unable to listen on ${port}, got ${e}`);
   }
+  console.log("Deno server started");
   for await (const conn of server) {
     // In order to not be blocking, we need to handle each connection individually
     // without awaiting the function
@@ -22,6 +23,7 @@ const startServer = async (port: number, timeout: number) => {
   }
 
   async function serveHttp(conn: Deno.Conn) {
+    console.log("serveHttp called);");
     // This "upgrades" a network connection into an HTTP connection.
     const httpConn = Deno.serveHttp(conn);
     // Each request sent over the HTTP connection will be yielded as an async
@@ -43,7 +45,9 @@ const startServer = async (port: number, timeout: number) => {
       } else if (
         requestEvent.request.method == "POST" && url.pathname == "/functions"
       ) {
+        console.log("/functions route called");
         const body = await requestEvent.request.text();
+
         try {
           // find the right function file to run
           const parsedPayload = ParseInvocationPayload(body);
