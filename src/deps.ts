b611fb9e8ef5fc04a5ea300ee53f0c552c0eab15
Fil Maj <maj.fil@gmail.com>
2023-03-21 08:50:26 -0400

    Update src/deps.ts
    Co-authored-by: Neil Shapiro <nshapiro91@gmail.com>

--- src/deps.ts
+++ src/deps.ts
@@ -1,9 +1,6 @@
 export { readAll } from "https://deno.land/std@0.99.0/io/util.ts";
 export { BaseSlackAPIClient } from "https://deno.land/x/deno_slack_api@0.0.2/base-client.ts";
-// TODO: can keep this dependency on deno_slack_builder until the next release, then switch this over to pull this from deno-slack-hooks.
-//       otherwise we'll have to be careful on the release timing of this repo and the hooks repo (have to both appear at the same time)
-//       luckily, there are no concerns about different protocols impacting the use of createManifest imported here, as the below is only
-//       used during local-run, which in the default/legacy protocol, logging to stdout is unlimited for that specific hook.
+// TODO: Update to import from `deno-slack-hooks` instead
 export { createManifest } from "https://deno.land/x/deno_slack_builder@0.0.14/manifest.ts";
 export { parse } from "https://deno.land/std@0.99.0/flags/mod.ts";
 export { getProtocolInterface } from "https://deno.land/x/deno_slack_protocols@0.0.2/mod.ts";
