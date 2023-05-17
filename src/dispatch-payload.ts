e08f1b7bf1c968eeedceaccbc8342e1eae380a1f
Filip Maj <fmaj@slack-corp.com>
2023-05-17 16:28:36 -0600

    deno fmt

--- src/dispatch-payload.ts
+++ src/dispatch-payload.ts
@@ -45,7 +45,7 @@ export const DispatchPayload = async (
   // TODO: should we check that this is a ValidEventType at runtime?
   const eventType: ValidEventType = payload?.body?.event?.type ||
     payload?.body?.type || "";
-    (payload?.body?.event?.type || payload?.body?.type || "") as ValidEventType;
+  (payload?.body?.event?.type || payload?.body?.type || "") as ValidEventType;
   const baseHandlerArgs = extractBaseHandlerArgsFromPayload(payload);
 
   const functionCallbackId = getFunctionCallbackID(eventType, payload);
