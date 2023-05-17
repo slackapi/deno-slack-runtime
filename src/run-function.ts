aee4e14f8d9b7ab3830e3c2feba986d67e19600f
Filip Maj <fmaj@slack-corp.com>
2023-05-17 16:24:41 -0600

    Expand on TODO/comment, and spread baseHandlerArgs first before tacking on `event` in run-function.

--- src/run-function.ts
+++ src/run-function.ts
@@ -6,7 +6,7 @@ export const RunFunction = async (
   baseHandlerArgs: BaseHandlerArgs,
   functionModule: FunctionModule,
 ): Promise<void> => {
-  // TODO: should we throw if this cannot be found?
+  // TODO: should we throw if this cannot be found? Alternatively / in addition, if calls below to complete* APIs fail, maybe we should throw then?
   const functionExecutionId = baseHandlerArgs.body.event?.function_execution_id;
 
   if (!functionModule.default) {
@@ -22,8 +22,8 @@ export const RunFunction = async (
   // We don't catch any errors the handlers may throw, we let them throw, and stop the process
   const { completed = true, outputs = {}, error } = await functionModule
     .default({
-      event: baseHandlerArgs.body.event,
       ...baseHandlerArgs,
+      event: baseHandlerArgs.body.event,
     });
 
   // App has indicated there's an unrecoverable error with this function invocation
