e3f31575e297544b22b468476588959e1a89549b
Junya Hayashi <junya.hayashi@groove-x.com>
2026-01-13 07:30:10 +0900

    fix: gather app manifest using deno-slack-hooks instead of deno-slack-builder (#74)
    * use deno-slack-hooks instead of deno-slack-builder
    
    * chore: formatting
    
    ---------
    
    Co-authored-by: Eden Zimbelman <eden.zimbelman@salesforce.com>

--- src/deps.ts
+++ src/deps.ts
@@ -1,8 +1,7 @@
 // TODO: latest version of std does not have a io/utils.ts
 export { readAll } from "https://deno.land/std@0.99.0/io/util.ts";
 export { BaseSlackAPIClient } from "https://deno.land/x/deno_slack_api@0.0.2/base-client.ts";
-// TODO: Update to import from `deno-slack-hooks` instead
-export { createManifest } from "https://deno.land/x/deno_slack_builder@0.0.14/manifest.ts";
+export { getManifest } from "https://deno.land/x/deno_slack_hooks@1.4.0/get_manifest.ts";
 export { parse } from "https://deno.land/std@0.99.0/flags/mod.ts";
 export { getProtocolInterface } from "https://deno.land/x/deno_slack_protocols@0.0.2/mod.ts";
 export type { Protocol } from "https://deno.land/x/deno_slack_protocols@0.0.2/types.ts";
